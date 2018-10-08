d68c2ea3ec118005595768d254d6b8fd378cad6e
Takuya Kosugiyama <re@itkq.jp>
2018-10-08 21:03:47 +0900

    Implement minimal evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,8 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/itkq/go-minruby-interpreter/evaluator"
 	"github.com/itkq/go-minruby-interpreter/lexer"
-	"github.com/itkq/go-minruby-interpreter/token"
+	"github.com/itkq/go-minruby-interpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -23,9 +24,25 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
