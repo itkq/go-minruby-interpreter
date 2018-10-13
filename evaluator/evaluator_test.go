279bffaabdf8d82818afe9432fb18652b2063059
Takuya Kosugiyama <re@itkq.jp>
2018-10-13 22:22:21 +0900

    Implement string concatenation for passing test6-2

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -223,6 +223,10 @@ func TestErrorHandling(t *testing.T) {
 			"unknown operator: BOOLEAN + BOOLEAN",
 		},
 		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
+		{
 			`
 if (10 > 1) {
   if (10 > 1) {
@@ -285,6 +289,20 @@ func TestStringLiteral(t *testing.T) {
 		t.Fatalf("String has wrong value. got=%q", str.Value)
 	}
 
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
 }
 
 func TestBuiltinFunctions(t *testing.T) {
